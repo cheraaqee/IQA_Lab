053aaab9c48d95ec05eb00f2635e14b7b19dbdd3
Pooryaa Cheraaqee <p.cheraaghi@gmail.com>
2021-03-27 20:04:07 +0430

    Update sctr_batch.m

--- IQA/Full_Reference/train_free/sctr_batch.m
+++ IQA/Full_Reference/train_free/sctr_batch.m
@@ -1,14 +1,10 @@
-% methods_list = {'psnr_index', 'ssim', 'GMSD', 'HaarPSI', 'WSC_3LHV','MDOGS', 'GFM', 'ESIM', 'SQMS', 'GSS', ...
-%     'SVQI', 'cgsi', 'SIQM'};
 addpath(genpath('./datasets'))
-methods_list = {'WS_3HV_D2'};
+% The plots will be saved in the folder of each method listed in the variable `methods_list'.
+% IT MUST BE NOTED that, you must have run the testing script prior to plotting the scatter plots.
+methods_list = {'GMSD'};
 for method_idx = 1:length(methods_list)
     evaluator = methods_list{method_idx};
     sctr_SCID(evaluator)
-    %        sctr_SIQAD(evaluator)
     sctr_QACS(evaluator)
     sctr_SIQAD(evaluator)
-    %     sctr_MLIVE(evaluator)
-    %     sctr_MDID2013(evaluator)
-    %     sctr_MDID(evaluator)
 end
